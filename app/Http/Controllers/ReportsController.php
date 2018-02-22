f80314d7022aef3832c733a36b280f223967b860
Denys Domanskyi <manager@div-art.com>
2018-02-22 14:01:41 +0200

    add filter functions to get reports method

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -5,15 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Message;
+use DB;
 
 class ReportsController extends Controller
 {
     public function get(Request $request)
     {
+        DB::enableQueryLog();
         $response = Message::whereDate('created_at', Carbon::parse($request->date)->toDateString());
         if ( ! empty($request->type)) {
             $response->where('type', $request->type);
         }
+
+        $ids = $request->ids;
+        $response->where(function($query) use ($ids) {
+            if ( ! empty($ids['dialog'])) {
+                $dialog_ids = $ids['dialog'];
+                $query->orWhere(function($q) use ($dialog_ids) {
+                    $q->where('type', 'dialog')->whereIn('target_id', $dialog_ids);
+                });
+            }
+
+            if ( ! empty($ids['review'])) {
+                $review_ids = $ids['review'];
+                $query->orWhere(function($q) use ($review_ids) {
+                    $q->where('type', 'review')->whereIn('target_id', $review_ids);
+                });
+            }
+
+            if ( ! empty($ids['alert'])) {
+                $alert_ids = $ids['alert'];
+                $query->orWhere(function($q) use ($alert_ids) {
+                    $q->where('type', 'alert')->whereIn('target_id', $alert_ids);
+                });
+            }
+        });
 
         if ( ! empty($request->phone)) {
             $phone = $request->phone;
